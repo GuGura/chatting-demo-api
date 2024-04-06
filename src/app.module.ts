dafb36e0cf574e13b8b8cc3bd3e623f721ca2165
pjy <wodus331@vanillacode.net>
2024-04-06 19:08:10 +0900

    main.ts 기본세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { SuthService } from './suth/suth.service';
 
 @Module({
   imports: [AuthModule],
   controllers: [AppController],
-  providers: [AppService, SuthService],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(json({ limit: '100mb' }));
+  app.use(urlencoded({ limit: '100mb', extended: true }));
+  app.use(cookieParser());
+  app.enableCors({
+    origin: /^(https?:\/\/)?(localhost|.*test\.net)(:[0-9]+)?$/,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(3500);
 }
+
 bootstrap();
