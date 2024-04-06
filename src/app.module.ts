66f0d642e3aa40ac86e913f38d0aad9fbb5ad630
pjy <wodus331@vanillacode.net>
2024-04-06 18:40:18 +0900

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { SuthService } from './suth/suth.service';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SuthService],
 })
 export class AppModule {}
