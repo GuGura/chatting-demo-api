2d40889da44ab894550f3c7195c637d9bbbe87ad
pjy <wodus331@vanillacode.net>
2024-04-11 17:10:10 +0900

    login error

--- src/auth/strategy/local.strategy.ts
+++ src/auth/strategy/local.strategy.ts
@@ -1,6 +1,6 @@
 import { Strategy } from 'passport-local';
 import { AuthGuard, PassportStrategy } from '@nestjs/passport';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { AuthService } from '../auth.service';
 
 @Injectable()
@@ -15,7 +15,8 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   async validate(email: string, password: string): Promise<any> {
     const user = await this.authService.validateUser({ email, password });
     if (!user) {
-      throw new UnauthorizedException();
+      console.log('log');
+      throw new BadRequestException('login fail');
     }
     return user;
   }
